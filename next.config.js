f3483fe508889d84d83cbf6fc2cf7d59ef796c33
PRANAV VERMA <pvverma0214@gmail.com>
2025-09-05 07:17:32 +0000

    Error: Invalid src prop (https://storage.googleapis.com/stately-temp-fil

--- next.config.js
+++ next.config.js
@@ -17,6 +17,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
